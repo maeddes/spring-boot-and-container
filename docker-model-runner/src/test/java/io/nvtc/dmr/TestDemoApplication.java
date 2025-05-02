6064a3b93e051b4ff59242f241af64e742cc95da
Matthias Haeussler <maeddes@gmail.com>
2025-05-02 18:18:30 +0000

    docker model runner

---
+++ docker-model-runner/src/test/java/io/nvtc/dmr/TestDemoApplication.java
@@ -0,0 +1,11 @@
+package io.nvtc.dmr;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
