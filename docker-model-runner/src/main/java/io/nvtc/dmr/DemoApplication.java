70aeee509ff65d66a3773d829c7b3609533dd85f
Matthias Haeussler <maeddes@gmail.com>
2025-05-03 10:16:21 +0200

    working configuration

--- docker-model-runner/src/main/java/io/nvtc/dmr/DemoApplication.java
+++ docker-model-runner/src/main/java/io/nvtc/dmr/DemoApplication.java
@@ -1,11 +1,29 @@
 package io.nvtc.dmr;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class DemoApplication {
+
+    private final ChatClient chatClient;
+
+    public DemoApplication(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return this.chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
