ee8b5bc280c64d3b0cb3142f367bbfd4dd215e7e
Matthias Haeussler <maeddes@gmail.com>
2025-05-21 16:16:55 +0200

    updates

--- kubernetes/src/main/java/com/springio/demo/kubernetes/controller/HelloController.java
+++ kubernetes/src/main/java/com/springio/demo/kubernetes/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.springio.demo.kubernetes.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${HOSTNAME:unset}")
+	String hostname;
+
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello from Spring! 👋";
+		return "Hello from Spring! 👋 My name is "+hostname;
 	}
 }
