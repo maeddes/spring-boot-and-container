e8ce1e22fa7b3f60bd3f48381aab1fdbc64881dc
Matthias Haeussler <maeddes@gmail.com>
2026-03-10 14:22:52 +0100

    print out model

--- ai-docker-model-runner-2.0/src/main/java/com/example/demo/DemoApplication.java
+++ ai-docker-model-runner-2.0/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,29 @@
 package com.example.demo;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
 	private final ChatClient chatClient;
+
+    @Autowired
+    Environment env;
+
+    @PostConstruct
+    public void dump() {
+        System.out.println("Using Model: " + env.getProperty("spring.ai.openai.chat.options.model"));
+    }
 
     public DemoApplication(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
